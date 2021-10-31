ca1fda5475d7b7daa88e0f3c2d5f98bd7e57c85b
Karql <karql.pl@gmail.com>
2021-10-31 10:01:48 +0100

    Improve AccountService tests

--- src/Services/KOMTracker.API.Tests/Infrastructure/Services/AccountServiceTests.cs
+++ src/Services/KOMTracker.API.Tests/Infrastructure/Services/AccountServiceTests.cs
@@ -25,9 +25,26 @@ namespace KOMTracker.API.Tests.Infrastructure.Services
 {
     public class AccountServiceTests
     {
-        private const string TEST_CODE = "xxx";
+        #region TestData
+        private const string TEST_INVALID_CODE = "invalid";
+        private const string TEST_EXISTING_ATHLETE_CODE = "exist";
+        private const string TEST_NEW_ATHLETE_CODE = "new";
         private const string TEST_INVALID_SCOPE = "read";
         private const string TEST_VALID_SCOPE = "read,activity:read,profile:read_all";
+
+        private AthleteModel TestExistingAthlete = new AthleteModel
+        {
+            AthleteId = 1,
+            Username = "Athlete1"
+        };
+        private AthleteModel TestNewAthlete = new AthleteModel
+        {
+            AthleteId = 2,
+            Username = "Athlete2"
+
+        };
+        private TokenModel TestToken = new TokenModel();
+        #endregion
 
         private readonly IMapper _mapper;
         private readonly IKOMUnitOfWork _komUoW;
@@ -49,6 +66,8 @@ namespace KOMTracker.API.Tests.Infrastructure.Services
             _userManager = Substitute.For<UserManager<UserModel>>(_userStore, null, null, null, null, null, null, null, null);
 
             _accountService = new AccountService(_mapper, _komUoW, _tokenService, _athleteService, _userManager);
+
+            PrepareMocks();
         }
 
         #region Connect
@@ -56,46 +75,87 @@ namespace KOMTracker.API.Tests.Infrastructure.Services
         public async Task Connect_check_is_scope_contains_required()
         {
             // Act
-            var res = await _accountService.Connect(TEST_CODE, TEST_INVALID_SCOPE);
+            var res = await _accountService.Connect(TEST_INVALID_CODE, TEST_INVALID_SCOPE);
 
             // Assert
             res.Should().BeFailure();
             res.HasError<ConnectError>(x => x.Message == ConnectError.NoRequiredScope).Should().BeTrue();
+
+            await AssertConnectNoUpdate();
         }
 
         [Fact]
         public async Task Connect_failed_when_invalid_code()
         {
-            // Arrange
-            _tokenService.ExchangeAsync(TEST_CODE, TEST_VALID_SCOPE).Returns(Result.Fail(new ExchangeError(ExchangeError.InvalidCode)));
-
             // Act
-            var res = await _accountService.Connect(TEST_CODE, TEST_VALID_SCOPE);
+            var res = await _accountService.Connect(TEST_INVALID_CODE, TEST_VALID_SCOPE);
 
             // Assert
             res.Should().BeFailure();
             res.HasError<ConnectError>(x => x.Message == ConnectError.InvalidCode).Should().BeTrue();
+
+            await AssertConnectNoUpdate();
         }
 
         [Fact]
-        public async Task Connect_add_or_update_athlete_and_token()
+        public async Task Connect_add_new_athlete()
         {
-            // Arrange
-            var users = new List<UserModel>()
-                .AsQueryable()
-                .BuildMock();
-            var athlete = new AthleteModel();
-            var token = new TokenModel();
-
-            _userManager.Users.Returns(users);
-            _tokenService.ExchangeAsync(TEST_CODE, TEST_VALID_SCOPE).Returns(Result.Ok((athlete, token)));
-
             // Act
-            var res = await _accountService.Connect(TEST_CODE, TEST_VALID_SCOPE);
+            var res = await _accountService.Connect(TEST_NEW_ATHLETE_CODE, TEST_VALID_SCOPE);
 
             // Assert
-            await _athleteService.Received().AddOrUpdateAthleteAsync(athlete);
-            await _athleteService.Received().AddOrUpdateTokenAsync(token);
+            await _athleteService.Received().AddOrUpdateAthleteAsync(TestNewAthlete);
+            await _athleteService.Received().AddOrUpdateTokenAsync(TestToken);
+            await _userManager.Received().CreateAsync(Arg.Is<UserModel>(x =>
+                x.AthleteId == TestNewAthlete.AthleteId
+                && x.UserName == TestNewAthlete.Username));
+            await _komUoW.Received().SaveChangesAsync();
+
+            res.Should().BeSuccess();
+        }
+
+        [Fact]
+        public async Task Connect_update_exsisting_athlete()
+        {
+            // Act
+            var res = await _accountService.Connect(TEST_EXISTING_ATHLETE_CODE, TEST_VALID_SCOPE);
+
+            // Assert
+            await _athleteService.Received().AddOrUpdateAthleteAsync(TestExistingAthlete);
+            await _athleteService.Received().AddOrUpdateTokenAsync(TestToken);
+            await _userManager.DidNotReceiveWithAnyArgs().CreateAsync(null);
+            await _komUoW.Received().SaveChangesAsync();
+
+            res.Should().BeSuccess();
+        }
+
+        private void PrepareMocks()
+        {
+            var users = new List<UserModel> { 
+                new UserModel { 
+                    AthleteId = TestExistingAthlete.AthleteId, 
+                    UserName = TestExistingAthlete.Username
+                }
+            }
+            .AsQueryable()
+            .BuildMock();
+            _userManager.Users.Returns(users);
+
+            _tokenService.ExchangeAsync(TEST_INVALID_CODE, TEST_VALID_SCOPE).Returns(Result.Fail(new ExchangeError(ExchangeError.InvalidCode)));
+
+            _tokenService.ExchangeAsync(TEST_INVALID_CODE, TEST_VALID_SCOPE).Returns(Result.Fail(new ExchangeError(ExchangeError.InvalidCode)));
+            
+            _tokenService.ExchangeAsync(TEST_EXISTING_ATHLETE_CODE, TEST_VALID_SCOPE).Returns(Result.Ok((TestExistingAthlete, TestToken)));
+
+            _tokenService.ExchangeAsync(TEST_NEW_ATHLETE_CODE, TEST_VALID_SCOPE).Returns(Result.Ok((TestNewAthlete, TestToken)));
+        }
+
+        private async Task AssertConnectNoUpdate()
+        {
+            await _athleteService.DidNotReceiveWithAnyArgs().AddOrUpdateAthleteAsync(null);
+            await _athleteService.DidNotReceiveWithAnyArgs().AddOrUpdateTokenAsync(null);
+            await _userManager.DidNotReceiveWithAnyArgs().CreateAsync(null);
+            await _komUoW.DidNotReceiveWithAnyArgs().SaveChangesAsync();
         }
         #endregion
     }
