8d3840b63e0695267945c919b0bca47c3744623d
Karql <karql.pl@gmail.com>
2021-12-01 21:31:10 +0100

    Add authentication to web

--- src/KomTracker/KomTracker.WEB/Program.cs
+++ src/KomTracker/KomTracker.WEB/Program.cs
@@ -9,6 +9,15 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+builder.Services.AddOidcAuthentication(options =>
+{
+    builder.Configuration.Bind("Local", options.ProviderOptions);
+    options.ProviderOptions.ResponseType = "code";
+    options.ProviderOptions.DefaultScopes.Add("openid");
+    options.ProviderOptions.DefaultScopes.Add("profile");
+    options.ProviderOptions.DefaultScopes.Add("api");
+});
+
 builder.Services.AddMudServices();
 
 await builder.Build().RunAsync();
