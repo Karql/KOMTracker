2df3c12d2fd3e1ec58954e46765856d6a5d51987
Karql <karql.pl@gmail.com>
2021-12-09 21:35:08 +0100

    Add koms page

---
+++ src/KomTracker/KomTracker.WEB/Pages/Koms.razor.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.Threading.Tasks;
+
+namespace KomTracker.WEB.Pages;
+
+public partial class Koms
+{
+    private bool _loaded = false;
+    private string _searchString = "";
+    private IEnumerable<KomViewModel> _koms;
+    private KomViewModel _kom;
+    protected override async Task OnInitializedAsync()
+    {
+        _koms = Enumerable.Range(1, 100).Select(x => new KomViewModel
+        {
+            SegmentId = x,
+            Name = $"Segment{x}"
+        }).ToList();          
+
+        _loaded = true;
+    }
+
+    private bool Search(KomViewModel kom)
+    {
+        if (string.IsNullOrWhiteSpace(_searchString)) return true;
+        if (kom.Name?.Contains(_searchString, StringComparison.OrdinalIgnoreCase) == true)
+        {
+            return true;
+        }
+        return false;
+    }
+}
+
+public class KomViewModel
+{
+    public long SegmentId { get; set; }
+
+    public string Name { get; set; }
+}
