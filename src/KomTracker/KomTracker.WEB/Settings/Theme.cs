050c28527202aec9da0ed8d92c363418261f5141
Karql <karql.pl@gmail.com>
2021-12-10 17:36:17 +0100

    Dark mode

---
+++ src/KomTracker/KomTracker.WEB/Settings/Theme.cs
@@ -0,0 +1,36 @@
+﻿using MudBlazor;
+
+namespace KomTracker.WEB.Settings;
+
+public class Theme
+{
+    public static readonly MudTheme DefaultTheme = new MudTheme()
+    {
+        Palette = new Palette()
+        {
+            Primary = "#FC4C02",
+            AppbarBackground = "#FC4C02",
+        }
+    };
+
+    public static readonly MudTheme DarkTheme = new MudTheme()
+    {
+        Palette = new Palette()
+        {
+            Primary = "#FC4C02",
+            AppbarBackground = "#373740",
+            AppbarText = "rgba(255,255,255, 0.70)",
+            Background = "#32333d",
+            BackgroundGrey = "#27272f",
+            Surface = "#373740",
+            DrawerIcon = "rgba(255,255,255, 0.50)",
+            DrawerBackground = "#27272f",
+            DrawerText = "rgba(255,255,255, 0.50)",
+            TextPrimary = "rgba(255,255,255, 0.70)",
+            TextSecondary = "rgba(255,255,255, 0.50)",
+            ActionDefault = "#adadb1",
+            ActionDisabled = "rgba(255,255,255, 0.26)",
+            ActionDisabledBackground = "rgba(255,255,255, 0.12)",
+        }
+    };
+}
---
+++ src/KomTracker/KomTracker.WEB/Shared/MainLayout.razor.cs
@@ -0,0 +1,30 @@
+﻿using KomTracker.WEB.Settings;
+using MudBlazor;
+
+namespace KomTracker.WEB.Shared;
+
+public partial class MainLayout
+{
+    bool _drawerOpen = true;
+    MudTheme _currentTheme = Theme.DefaultTheme;
+
+    protected override async Task OnInitializedAsync()
+    {
+        _currentTheme = Theme.DefaultTheme;
+    }
+
+    void DrawerToggle()
+    {
+        _drawerOpen = !_drawerOpen;
+    }
+
+    private List<BreadcrumbItem> _items = new List<BreadcrumbItem>
+    {
+        new BreadcrumbItem("Dashboard", href: "#"),
+    };
+
+    private async Task DarkMode()
+    {
+        _currentTheme = _currentTheme == Theme.DefaultTheme ? Theme.DarkTheme : Theme.DefaultTheme;
+    }
+}
