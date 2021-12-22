56531c56ed0665c848cc1f2159b265f1a2d8765b
Karql <karql.pl@gmail.com>
2021-12-22 22:24:50 +0100

    Fix configuration

--- src/KomTracker/KomTracker.WEB/DependencyInjection.cs
+++ src/KomTracker/KomTracker.WEB/DependencyInjection.cs
@@ -4,15 +4,25 @@ using KomTracker.WEB.Infrastructure.Services.User;
 using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
+using IdentityConstants = KomTracker.Infrastructure.Shared.Identity.Constants;
 
 namespace KomTracker.WEB;
 
 public static class DependencyInjection
 {
     private const string ClientName = "KomTracker.API";
+    private const string ConfigurationKey_KomTrackerApiUrl = "KomTrackerApiUrl";
 
     public static WebAssemblyHostBuilder AddClientServices(this WebAssemblyHostBuilder builder)
     {
+        // TODO: ConfigurationService
+        var komTrackerApiUrl = builder.Configuration[ConfigurationKey_KomTrackerApiUrl];
+
+        if (komTrackerApiUrl == null)
+        {
+            throw new Exception($"Invalid configuration \"{ConfigurationKey_KomTrackerApiUrl}\" should not be null!");
+        }
+
         var services = builder.Services;
 
         services.AddScoped<IPreferenceService, PreferenceService>();
@@ -27,20 +37,20 @@ public static class DependencyInjection
             options.ProviderOptions.ResponseType = "code";
             options.ProviderOptions.DefaultScopes.Add("openid");
             options.ProviderOptions.DefaultScopes.Add("profile");
-            options.ProviderOptions.DefaultScopes.Add("api");
+            options.ProviderOptions.DefaultScopes.Add(IdentityConstants.OAuth2.ScopeApi);
             options.ProviderOptions.DefaultScopes.Add("offline_access");
         });
 
         services.AddHttpClient(ClientName, client =>
         {
-            client.BaseAddress = new Uri("https://localhost:9999/kom-tracker-api/");
+            client.BaseAddress = new Uri(komTrackerApiUrl);
         })
         .AddHttpMessageHandler(sp =>
         {
             var handler = sp.GetService<AuthorizationMessageHandler>()!
             .ConfigureHandler(
-                authorizedUrls: new[] { "https://localhost:9999/kom-tracker-api/identity" },
-                scopes: new[] { "api" }
+                authorizedUrls: new[] { komTrackerApiUrl },
+                scopes: new[] { IdentityConstants.OAuth2.ScopeApi }
             );
 
             return handler;
