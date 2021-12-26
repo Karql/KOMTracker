a7773af129aaf3ef2d187033693e9eb0a18b2c55
Karql <karql.pl@gmail.com>
2021-12-26 13:58:34 +0100

    PersonCard information from claims

---
+++ src/KomTracker/KomTracker.WEB/Shared/PersonCard.razor.cs
@@ -0,0 +1,22 @@
+﻿using KomTracker.WEB.Infrastructure.Services.User;
+using KomTracker.WEB.Models.User;
+using Microsoft.AspNetCore.Components;
+
+namespace KomTracker.WEB.Shared;
+
+public partial class PersonCard
+{
+    [Parameter] public string Class { get; set; }
+    [Parameter] public string Style { get; set; }
+
+    private bool _loaded = false;
+    private UserModel _user = default!;
+
+    [Inject]
+    private IUserService UserService { get; set; } = default!;
+    protected override async Task OnInitializedAsync()
+    {
+        _user = await UserService.GetCurrentUser();
+        _loaded = true;
+    }
+}
