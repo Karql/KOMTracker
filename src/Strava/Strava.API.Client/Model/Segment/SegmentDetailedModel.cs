10e06b8f6aa6e2810705e1767447b2d8c643d5b8
Karql <karql.pl@gmail.com>
2022-01-24 20:42:52 +0100

    Add SegmentDetailedModel

---
+++ src/Strava/Strava.API.Client/Model/Segment/SegmentDetailedModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Strava.API.Client.Model.Segment;
+
+public  class SegmentDetailedModel : SegmentSummaryModel
+{
+    [JsonPropertyName("created_at")]
+    public DateTime CreatedAt { get; set; }
+
+    [JsonPropertyName("updated_at")]
+    public DateTime UpdatedAt { get; set; }
+
+    [JsonPropertyName("total_elevation_gain")]
+    public float? TotalElevationGain { get; set; }
+    
+    [JsonPropertyName("effort_count")]
+    public int EffortCount { get; set; }
+
+    [JsonPropertyName("athlete_count")]
+    public int AthleteCount { get; set; }
+
+    [JsonPropertyName("star_count")]
+    public int StarCount { get; set; }
+
+    /*
+     * Not needed properties for now
+     * map
+     * athlete_segment_stats
+     * xoms
+     * local_legend
+     */
+}
