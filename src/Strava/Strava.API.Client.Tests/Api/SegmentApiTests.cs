28d2e3bde37b846e366ee015be667b2b02b38ad9
Karql <karql.pl@gmail.com>
2022-01-30 09:17:32 +0100

    Add Get_segment_returns_not_found_on_404 test

--- src/Strava/Strava.API.Client.Tests/Api/SegmentApiTests.cs
+++ src/Strava/Strava.API.Client.Tests/Api/SegmentApiTests.cs
@@ -4,6 +4,7 @@ using FluentResults.Extensions.FluentAssertions;
 using RichardSzalay.MockHttp;
 using Strava.API.Client.Api;
 using Strava.API.Client.Model.Segment;
+using Strava.API.Client.Model.Segment.Error;
 using Strava.API.Client.Tests.Extensions.Model.Segment;
 using System;
 using System.Collections.Generic;
@@ -28,6 +29,7 @@ public class SegmentApiTests
 
     #region TestData
     private const int TEST_SEGMENT_ID = 1;
+    private const int TEST_SEGMENT_NOT_EXISTS_ID = 404;
     private const string TEST_VALID_TOKEN = "token123";
     #endregion
 
@@ -62,6 +64,25 @@ public class SegmentApiTests
         actualSegment.Should().BeEquivalentTo(expectedSegment);
 
         _mockHttp.VerifyNoOutstandingExpectation();
+    }
+
+    [Fact]
+    public async Task Get_segment_returns_not_found_on_404()
+    {
+        // Arrange
+        _mockHttp.Expect(HttpMethod.Get, GetSegmentUrl(TEST_SEGMENT_NOT_EXISTS_ID))
+            .WithHeaders("Authorization", $"Bearer {TEST_VALID_TOKEN}")
+            .Respond(HttpStatusCode.NotFound);
+
+        // Act
+        var res = await _segmentApi.GetSegmentAsync(TEST_SEGMENT_NOT_EXISTS_ID, TEST_VALID_TOKEN);
+
+        // Assert
+        res.Should().BeFailure();
+        res.HasError<GetSegmentError>(x => x.Message == GetSegmentError.NotFound).Should().BeTrue();
+
+        _mockHttp.VerifyNoOutstandingExpectation();
+        _logger.CheckLogWarning("Not found!");
     }
 
     private string GetSegmentUrl(long segmentId)
