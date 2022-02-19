c13586418a79ed945cd1694236b2f6b148a4190e
Karql <karql.pl@gmail.com>
2022-02-19 12:34:45 +0100

    Add BaseEntity

---
+++ src/KomTracker/KomTracker.Domain/Contracts/BaseEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KomTracker.Domain.Contracts;
+
+public class BaseEntity
+{
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+}
