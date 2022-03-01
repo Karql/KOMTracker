1c4064186d173c820e6b6ae8a3acead33dd3a38e
Karql <karql.pl@gmail.com>
2022-03-01 08:29:07 +0100

    Add RefreshSegmentsCommand

---
+++ src/KomTracker/KomTracker.Application/Commands/Segment/RefreshSegmentsCommand.cs
@@ -0,0 +1,17 @@
+﻿using FluentResults;
+using MediatR;
+
+namespace KomTracker.Application.Commands.Tracking;
+
+public class RefreshSegmentsCommand : IRequest<Result>
+{
+
+}
+
+public class RefreshSegmentsCommandHandler : IRequestHandler<RefreshSegmentsCommand, Result>
+{
+    public Task<Result> Handle(RefreshSegmentsCommand request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
