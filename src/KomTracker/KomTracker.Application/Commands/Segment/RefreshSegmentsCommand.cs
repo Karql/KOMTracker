84e97a44dda7e327d06c2d037e6b16e526c9b760
Karql <karql.pl@gmail.com>
2022-03-06 09:45:47 +0100

    Extend RefreshSegmentsCommandHandler

--- src/KomTracker/KomTracker.Application/Commands/Segment/RefreshSegmentsCommand.cs
+++ src/KomTracker/KomTracker.Application/Commands/Segment/RefreshSegmentsCommand.cs
@@ -1,4 +1,5 @@
 ﻿using FluentResults;
+using KomTracker.Application.Models.Configuration;
 using KomTracker.Application.Services;
 using MediatR;
 
@@ -11,15 +12,18 @@ public class RefreshSegmentsCommand : IRequest<Result>
 
 public class RefreshSegmentsCommandHandler : IRequestHandler<RefreshSegmentsCommand, Result>
 {
+    private readonly ApplicationConfiguration _applicationConfiguration;
     private readonly ISegmentService _segmentService;
 
-    public RefreshSegmentsCommandHandler(ISegmentService segmentService)
+    public RefreshSegmentsCommandHandler(ApplicationConfiguration applicationConfiguration, ISegmentService segmentService)
     {
+        _applicationConfiguration = applicationConfiguration ?? throw new ArgumentNullException(nameof(applicationConfiguration));
         _segmentService = segmentService ?? throw new ArgumentNullException(nameof(segmentService));
     }
 
     public async Task<Result> Handle(RefreshSegmentsCommand request, CancellationToken cancellationToken)
     {
+        var masterStravaAthleteId = _applicationConfiguration.MasterStravaAthleteId;
         var segments = await _segmentService.GetSegmentsToRefreshAsync();
 
         throw new NotImplementedException();
