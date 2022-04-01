9ffd6647c4d59d452c2d55e6b1f2f890d3041a4f
Karql <karql.pl@gmail.com>
2022-04-01 21:08:49 +0200

    Add SendinBlueConfiguration

--- src/KomTracker/KomTracker.Application/Models/Configuration/ApplicationConfiguration.cs
+++ src/KomTracker/KomTracker.Application/Models/Configuration/ApplicationConfiguration.cs
@@ -6,10 +6,17 @@ using System.Threading.Tasks;
 
 namespace KomTracker.Application.Models.Configuration;
 
+public class SendinBlueConfiguration
+{
+    public string? ApiKey { get; set; }
+}
+
 public class ApplicationConfiguration
 {
     /// <summary>
     /// Athlete id using to queries for data like segment details
     /// </summary>
     public int MasterStravaAthleteId { get; set; }
+
+    public SendinBlueConfiguration? SendinBlueConfiguration { get; set; }
 }
