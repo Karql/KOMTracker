7e52d3e0d7b9bf8f9adc938c8bd16f3dec7c552a
Karql <karql.pl@gmail.com>
2022-05-16 20:45:23 +0200

    Change cert notify email

--- deployments/prod-reverse-proxy/get_certs.sh
+++ deployments/prod-reverse-proxy/get_certs.sh
@@ -16,7 +16,7 @@ fi
 domains=(komtracker.karkula.pl)
 rsa_key_size=4096
 data_path="/u01/volumes/reverse-proxy/certbot"
-email="mateusz@karkula.pl" # Adding a valid address is strongly recommended
+email="karql.pl@gmail.com" # Adding a valid address is strongly recommended
 staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
