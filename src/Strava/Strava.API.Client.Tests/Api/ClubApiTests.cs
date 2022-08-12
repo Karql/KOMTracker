c8d9b7a1cc95b9974cff5a891416500b923d0bd0
Karql <karql.pl@gmail.com>
2022-08-12 23:13:34 +0200

    Add tests for ClubApi

---
+++ src/Strava/Strava.API.Client.Tests/Api/ClubApiTests.cs
@@ -0,0 +1,153 @@
+﻿using AutoFixture;
+using FluentAssertions;
+using RichardSzalay.MockHttp;
+using Strava.API.Client.Api;
+using Strava.API.Client.Model.Segment;
+using Strava.API.Client.Tests.Extensions.Model.Segment;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Mime;
+using System.Text;
+using System.Threading.Tasks;
+using Utils.Tests.HttpClient;
+using Utils.Tests.Logging;
+using Xunit;
+using FluentResults.Extensions.FluentAssertions;
+using Strava.API.Client.Model.Base;
+using Strava.API.Client.Tests.Extensions.Model.Base;
+using Strava.API.Client.Tests.Common;
+using Strava.API.Client.Model.Club;
+using Strava.API.Client.Tests.Extensions.Model.Club;
+
+namespace Strava.API.Client.Tests.Api;
+
+public class ClubApiTests
+{
+    private readonly ITestLogger<ClubApi> _logger;
+    private readonly MockHttpMessageHandler _mockHttp;
+
+    private readonly IClubApi _clubApi;
+
+    #region TestData
+    private const string TEST_TOKEN_VALID = "token123";
+    private const string TEST_TOKEN_INVALID = "tokeninvalid123";
+    #endregion
+
+    public ClubApiTests(ITestLogger<ClubApi> logger)
+    {
+        _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        _mockHttp = new MockHttpMessageHandler();
+
+        _clubApi = new ClubApi(_logger, _mockHttp.ToHttpClientFactory());
+    }
+
+    #region Get clubs
+    [Theory]
+    [InlineData(0)]
+    [InlineData(1)]
+    [InlineData(2)]
+    [InlineData(3)]
+    public async Task Get_clubs_iterates_through_all_pages_and_returns_list(int pageCount)
+    {
+        // Arrange
+        var expectedClubs = new List<ClubSummaryModel>();
+
+        var fixture = FixtureHelper.GetTestFixture();
+
+        for (int pageNumber = 1; pageNumber <= pageCount; ++pageNumber)
+        {
+            var pageClubs = fixture.CreateMany<ClubSummaryModel>(5);
+
+            _mockHttp.Expect(HttpMethod.Get, GetClubsUrl(pageNumber))
+                .WithHeaders("Authorization", $"Bearer {TEST_TOKEN_VALID}")
+                .Respond(HttpStatusCode.OK, MediaTypeNames.Application.Json, pageClubs.ToJson());
+
+            expectedClubs.AddRange(pageClubs);
+        }
+
+        // empty page at the end
+        _mockHttp.Expect(HttpMethod.Get, GetClubsUrl(pageCount + 1))
+            .WithHeaders("Authorization", $"Bearer {TEST_TOKEN_VALID}")
+            .Respond(HttpStatusCode.OK, MediaTypeNames.Application.Json, Enumerable.Empty<ClubSummaryModel>().ToJson());
+
+        // Act
+        var res = await _clubApi.GetClubsAsync(TEST_TOKEN_VALID);
+
+        // Assert
+        res.Should().BeSuccess();
+        var actualClubs = res.Value;
+
+        actualClubs.Should().BeEquivalentTo(expectedClubs);
+
+        _mockHttp.VerifyNoOutstandingExpectation();
+    }
+
+    [Theory]
+    [InlineData(1)]
+    [InlineData(2)]
+    [InlineData(3)]
+    public async Task Get_clubs_stops_iterating_and_returns_error(int errorOnPage)
+    {
+        // Arrange
+        var fixture = FixtureHelper.GetTestFixture();
+
+        for (int pageNumber = 1; pageNumber < errorOnPage; ++pageNumber)
+        {
+            var pageClubs = fixture.CreateMany<ClubSummaryModel>(5);
+
+            _mockHttp.Expect(HttpMethod.Get, GetClubsUrl(pageNumber))
+                .Respond(HttpStatusCode.OK, MediaTypeNames.Application.Json, pageClubs.ToJson());
+        }
+
+        _mockHttp.Expect(HttpMethod.Get, GetClubsUrl(errorOnPage))
+            .Respond(HttpStatusCode.BadRequest);
+
+        // Act
+        var res = await _clubApi.GetClubsAsync(TEST_TOKEN_VALID);
+
+        // Assert
+        res.Should().BeFailure();
+        _mockHttp.VerifyNoOutstandingExpectation();
+        _logger.CheckLogError("failed! SatusCode");
+    }
+
+    [Fact]
+    public async Task Get_clubs_returns_unauthorized_on_401()
+    {
+        // Arrange
+        _mockHttp.Expect(HttpMethod.Get, GetClubsUrl(1))
+            .WithHeaders("Authorization", $"Bearer {TEST_TOKEN_INVALID}")
+            .Respond(HttpStatusCode.Unauthorized);
+
+        // Act
+        var res = await _clubApi.GetClubsAsync(TEST_TOKEN_INVALID);
+
+        // Arrange
+        res.Should().BeFailure();
+        _mockHttp.VerifyNoOutstandingExpectation();
+        _logger.CheckLogWarning("Unauthorized!");
+    }
+
+    [Fact]
+    public async Task Get_clubs_throws_exception_when_something_went_wrong()
+    {
+        // Arrange
+        _mockHttp.Expect(HttpMethod.Get, GetClubsUrl(1))
+            .Throw(new Exception("Something went wrong"));
+
+        // Act
+        Func<Task> action = () => _clubApi.GetClubsAsync(TEST_TOKEN_VALID);
+
+        // Assert
+        await action.Should().ThrowAsync<Exception>();
+    }
+
+    private string GetClubsUrl(int page)
+    {
+        return $"https://www.strava.com/api/v3/athlete/clubs?per_page=200&page={page}"; // 200 max possible
+    }
+    #endregion
+}
---
+++ src/Strava/Strava.API.Client.Tests/Extensions/Model/Club/ClubSummaryModelExtensions.cs
@@ -0,0 +1,55 @@
+﻿using Strava.API.Client.Model.Club;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Utils.Extensions;
+
+namespace Strava.API.Client.Tests.Extensions.Model.Club;
+public static class ClubSummaryModelExtensions
+{
+    /// <summary>
+    /// JSON equivalent to API response
+    /// </summary>
+    /// <remarks>
+    /// Manually created string string for testing deserialization
+    /// </remarks>
+    public static string ToJson(this ClubSummaryModel model)
+    {
+        return @$"{{
+            ""id"": {model.Id},
+            ""resource_state"": {(int)model.ResourceState},
+            ""name"": ""{model.Name}"",
+            ""profile_medium"": ""{model.ProfileMedium}"",
+            ""profile"": ""{model.Profile}"",
+            ""cover_photo"": ""{model.CoverPhoto}"",
+            ""cover_photo_small"": ""{model.CoverPhotoSmall}"",
+            ""activity_types"": [{string.Join(", ", model.ActivityTypes.Select(x => '"' + x + '"'))}],
+            ""activity_types_icon"": ""{model.ActivityTypesIcon}"",
+            ""dimensions"": [{string.Join(", ", model.Dimensions.Select(x => '"' + x + '"'))}],
+            ""sport_type"": ""{model.SportType}"",
+            ""city"": ""{model.City}"",
+            ""state"": ""{model.State}"",
+            ""country"": ""{model.Country}"",
+            ""private"": {model.Private.ToLowerString()},
+            ""member_count"": {model.MemberCount},
+            ""featured"": {model.Featured.ToLowerString()},
+            ""verified"": {model.Verified.ToLowerString()},
+            ""url"": ""{model.Url}""
+        }}";
+    }
+
+    /// <summary>
+    /// JSON equivalent to API response
+    /// </summary>
+    /// <remarks>
+    /// Manually created string string for testing deserialization
+    /// </remarks>
+    public static string ToJson(this IEnumerable<ClubSummaryModel> list)
+    {
+        return @$"[
+                {string.Join(", ", list.Select(x => x.ToJson()))}
+            ]";
+    }
+}
